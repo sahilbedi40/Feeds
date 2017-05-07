61bc441e8689c2b148f6494f6e7e86a2fdd05142
sahilbedi40 <er.sahil.1991@gmail.com>
2017-05-08 00:33:34 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {AuthenticateService} from  './Services/authenticate.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+  userName:string;
+  imgUrl:string;
+  constructor(protected route:Router,protected authService:AuthenticateService){
+    authService.isAuthenticate=false;
+    this.userName ="test";
+    this.authService.userInfo.subscribe(userDetails => {
+      this.userName = userDetails.Name;
+      this.imgUrl=userDetails.PhotoUrl !="" ?userDetails.PhotoUrl:"../assets/images/noimage.jpg";
+    })
+  }
+
+   LogOut(){
+     this.authService.LogOut();
+    this.route.navigate(["./Login"]);
+   }
+
 }
